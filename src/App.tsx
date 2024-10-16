80af114604aef78aea832a51a735ce19fe0dd420
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-16 10:15:01 +0200

    create function to change state and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => {
+      if (prevPage === 3) {
+        return 1;
+      } else {
+        return prevPage + 1;
+      }
+    });
+  };
+
+  return (
+    <>
+      <main>
+        {currentPage === 1 && (
+          <div>
+            <h2>Page 1</h2>
+            <p>1.</p>
+          </div>
+        )}
+
+        {currentPage === 2 && (
+          <div>
+            <h2>Page 2</h2>
+            <p>2</p>
+          </div>
+        )}
+
+        {currentPage === 3 && (
+          <div>
+            <h2>Page 3</h2>
+            <p>3</p>
+          </div>
+        )}
+      </main>
+      <button onClick={nextPage}>Page suivante</button>
+    </>
+  );
 }
 
 export default App;
