36af6d598135a3fd0c76f70359e55a1456556019
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-18 22:26:57 +0200

    create style about bg, main and header of main

--- src/App.tsx
+++ src/App.tsx
@@ -38,31 +38,39 @@ function App({ formData, setFormData }: FormProps) {
   };
 
   return (
-    <>
-      <main>
-        {currentPage === 1 && <Page1 />}
+    <div className="font-custom bg-[url('./assets/bg-img.png')] bg-cover w-full p-0 py-40 flex justify-center items-center">
+      <main className="bg-white p-8">
+        <section>
+          <header className="flex flex-col justify-center items-center gap-4">
+            <h1 className="text-3xl font-semibold">Registration Form</h1>
+            <p className="text-sm text-zinc-300 ">
+              Please fill out this form with the required information
+            </p>
+          </header>
+          {currentPage === 1 && <Page1 />}
 
-        {currentPage === 2 && (
-          <div>
-            <Page2 handleSubmit={handleSubmit} />
-          </div>
-        )}
+          {currentPage === 2 && (
+            <div>
+              <Page2 handleSubmit={handleSubmit} />
+            </div>
+          )}
 
-        {currentPage === 3 && (
-          <div>
-            <h2>Page 3</h2>
-          </div>
-        )}
+          {currentPage === 3 && (
+            <div>
+              <h2>Page 3</h2>
+            </div>
+          )}
+
+          {currentPage === 1 || currentPage === 2 ? (
+            <button onClick={nextPage}>Page suivante</button>
+          ) : (
+            <button onClick={handleSubmit} type="submit">
+              Send
+            </button>
+          )}
+        </section>
       </main>
-
-      {currentPage === 1 || currentPage === 2 ? (
-        <button onClick={nextPage}>Page suivante</button>
-      ) : (
-        <button onClick={handleSubmit} type="submit">
-          Send
-        </button>
-      )}
-    </>
+    </div>
   );
 }
 
